cafcd5ca5604ae1418b0b7589ff586527b723fa6
Jay Patel <jnpatel2811@gmail.com>
2019-03-14 22:58:43 +0530

    Initial commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_midi/FlutterMidiPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterMidiPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterMidiPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
